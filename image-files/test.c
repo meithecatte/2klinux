6d38f4acdac8c3b08957c574eb51bf617050054f
NieDzejkob <niedzejkob@gmail.com>
2018-03-07 18:57:00 +0100

    Preprocessing conditionals (without expression evalutation)

--- image-files/test.c
+++ image-files/test.c
@@ -1,3 +1,20 @@
-Lorem ipsum
-dolor sit amet
-I like trains
+#if TRUE
+one
+two
+three
+#elif TRUE
+four
+five
+six
+#elif FALSE
+seven
+eight
+nine
+#else
+ten
+eleven
+twelve
+#endif
+end one
+end two
+end three
